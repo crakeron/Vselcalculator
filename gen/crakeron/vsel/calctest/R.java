3e0e3faafa07f2f25072b67769e415a7dec8971a
Marc Rosser <crakeronmarc@caramail.com>
2012-07-25 16:32:20 +0200

    Working on a Linear layout, more friendly to build
    New layout is in Main2.xml. To be continued to resemble main.xml
    -Added Spinner for selecting 3/4Vsels
    -Added credits

--- gen/crakeron/vsel/calctest/R.java
+++ gen/crakeron/vsel/calctest/R.java
@@ -20,29 +20,37 @@ public final class R {
         public static final int calc_go=0x7f050004;
         public static final int column1=0x7f05000f;
         public static final int column2=0x7f05000c;
+        public static final int credits=0x7f050012;
         public static final int editText1=0x7f050000;
         public static final int editText2=0x7f050001;
         public static final int editText3=0x7f050002;
         public static final int mhz1=0x7f050008;
         public static final int mhz2=0x7f050009;
         public static final int mhz3=0x7f05000a;
+        public static final int spinner1=0x7f050014;
+        public static final int textView1=0x7f050013;
         public static final int textView2=0x7f050005;
         public static final int textView3=0x7f050006;
         public static final int textView4=0x7f050007;
+        public static final int textView5=0x7f050015;
         public static final int voltage1=0x7f05000b;
         public static final int voltage2=0x7f050011;
         public static final int voltage3=0x7f050010;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int main2=0x7f030001;
     }
     public static final class string {
         public static final int Column1=0x7f040000;
         public static final int app_name=0x7f040001;
         public static final int arrow=0x7f040003;
         public static final int calc=0x7f040004;
+        public static final int credits=0x7f040007;
         public static final int hello=0x7f040002;
         public static final int mhz=0x7f040005;
+        public static final int spinner=0x7f040008;
         public static final int voltage=0x7f040006;
+        public static final int welcome=0x7f040009;
     }
 }
