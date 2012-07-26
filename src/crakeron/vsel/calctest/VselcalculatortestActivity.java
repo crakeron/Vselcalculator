04dad0a8e2d7f2b3f0faec81c5e81cb0ea00b3c0
Marc Rosser <crakeronmarc@caramail.com>
2012-07-26 19:25:48 +0200

    Code cleanup + increase app version to v0.5

--- src/crakeron/vsel/calctest/VselcalculatortestActivity.java
+++ src/crakeron/vsel/calctest/VselcalculatortestActivity.java
@@ -4,13 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
-
-
 
 public class VselcalculatortestActivity extends Activity {
 	private EditText freqbox1;
@@ -21,8 +18,19 @@ public class VselcalculatortestActivity extends Activity {
 	private EditText voltbox2;
 	private EditText voltbox3;
 	private EditText voltbox4;
-	public Spinner spinner;
-	public boolean freq4ornot=false;
+	private Spinner spinner;
+	
+	public int freq1;
+    public int freq2;
+    public int freq3;
+    public int freq4;
+    public int volt1;
+    public int volt2;
+    public int volt3;
+    public int volt4;
+    
+    public boolean stop=false;
+    public boolean freq4ornot=false;
 	
     /** Called when the activity is first created. */
     @Override
@@ -38,53 +46,28 @@ public class VselcalculatortestActivity extends Activity {
         voltbox2 = (EditText) findViewById(R.id.volt2);
         voltbox3 = (EditText) findViewById(R.id.volt3);
         voltbox4 = (EditText) findViewById(R.id.volt4);
+        
         spinner = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+        R.array.spinner_choices, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
         
-        
-     
-     ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-             R.array.spinner_choices, android.R.layout.simple_spinner_item);
-     // Specify the layout to use when the list of choices appears
-     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-     // Apply the adapter to the spinner
-     spinner.setAdapter(adapter);
-     spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
-    	 
-    	 public void onItemSelected(AdapterView <?> adapter, View v, int pos, long lng) {
-         	if (pos==0){freq4ornot=false;}
-         	if (pos==1){freq4ornot=true;}
-         	
-         	Toast.makeText(getApplicationContext(),"freq4ornot bool is " + freq4ornot, Toast.LENGTH_SHORT).show();
-    	 }
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+        			public void onItemSelected(AdapterView <?> adapter, View v, int pos, long lng) {
+        				if (pos==0){freq4ornot=false;}
+        				if (pos==1){freq4ornot=true;}
+        				Toast.makeText(getApplicationContext(),"freq4ornot bool is " + freq4ornot, Toast.LENGTH_SHORT).show();
+        			}
     	  
-    	 public void onNothingSelected(AdapterView arg0) {
-    	 //do something else
-    	 }
-    	 });
-     
-     
-     
+        			public void onNothingSelected(AdapterView <?> arg0) {
+        				//nothing ATM
+        			}
+        });
     }
-          
-    
-     
-     
-     /*String num_freq = spinner.getSelectedItem().toString();*/
-     
-    
-    public int freq1;
-    public int freq2;
-    public int freq3;
-    public int freq4;
-    public int volt1;
-    public int volt2;
-    public int volt3;
-    public int volt4;
-    
-    public boolean stop=false;
-    
-    
-    
+ 
     
     public void button_pressed(View button) { 
     	voltbox1.setText("");
@@ -92,54 +75,47 @@ public class VselcalculatortestActivity extends Activity {
     	voltbox3.setText("");
     	voltbox4.setText("");
     	stop=false;
-    	
-    	
-    	
-    	//What to do when calculate button is pressed:
     	// 1. Grab values in textboxes freq1,2,3 (and 4, depending on Spinner value?) and store their values
     	grab_values(freq4ornot);
     	// 2. Call calculate function with 3 (or 4) arguments
-    	
     	calculate(freq1, freq2, freq3, freq4, freq4ornot);
     	// 3. call function to display each result in correct box, if stop=true, then all boxes will be displayed empty
     	display_volt(freq4ornot);
     	if(stop==true) error_empty();
-    	
-    	
-    	
     }
     
     public void grab_values(boolean freq4ornot){
-    	
     	String freq1Value = freqbox1.getText().toString();//fetch what's in edittextbox and store it in a string
     	if(freq1Value.length()!=0){
-      	freq1 = Integer.parseInt(freq1Value);//transform the string into an int and store it in our variable
-    	}
+    		freq1 = Integer.parseInt(freq1Value);//transform the string into an int and store it in our variable
+    		}
     	if(freq1Value.length()==0){
     		stop=true;
     		freq1=0;
-    	}
+    		}
     	
     	String freq2Value = freqbox2.getText().toString();
     	if(freq2Value.length()!=0){
           	freq2 = Integer.parseInt(freq2Value);//transform the string into an int and store it in our variable
         	}
         	if(freq2Value.length()==0){//check if user entered a value in box, otherwise causes crash
-        		stop=true;        		  		
-        		freq2=0;
+        	stop=true;        		  		
+        	freq2=0;
         	}
+        	
     	String freq3Value = freqbox3.getText().toString();
     	if(freq3Value.length()!=0){
-          	freq3 = Integer.parseInt(freq3Value);//transform the string into an int and store it in our variable
+          	freq3 = Integer.parseInt(freq3Value);
         	}
         	if(freq3Value.length()==0){
         		stop=true;        		     	
         		freq3=0;
         	}
+        	
     	if (freq4ornot==true){
     		String freq4Value = freqbox4.getText().toString();
     		if(freq4Value.length()!=0){
-    	      	freq4 = Integer.parseInt(freq4Value);//transform the string into an int and store it in our variable
+    	      	freq4 = Integer.parseInt(freq4Value);
     	    	}
     	    	if(freq4Value.length()==0){
     	    		stop=true;    	    		           		
@@ -152,9 +128,7 @@ public class VselcalculatortestActivity extends Activity {
     	volt1 = formula(freq1);
     	volt2 = formula(freq2);
     	volt3 = formula(freq3);
-        if (freq4ornot==true){
-        	volt4 = formula(freq4);
-           }
+        if (freq4ornot==true){volt4 = formula(freq4);}
     }
     
     
@@ -169,10 +143,8 @@ public class VselcalculatortestActivity extends Activity {
     		voltbox1.setText(String.valueOf(volt1));
     		voltbox2.setText(String.valueOf(volt2));
     		voltbox3.setText(String.valueOf(volt3));
-    		if(freq4ornot==true){
-    			voltbox4.setText(String.valueOf(volt4));
-    			}
-    		}
+    		if(freq4ornot==true){voltbox4.setText(String.valueOf(volt4));}
+    	}
     	
     }
 
@@ -181,26 +153,9 @@ public class VselcalculatortestActivity extends Activity {
     	Toast.makeText(getApplicationContext(), "Please enter a frequency in all the boxes", Toast.LENGTH_LONG).show();
     	/* For debug purposes
     	Toast.makeText(getApplicationContext(),"stop bool is " + stop, Toast.LENGTH_LONG).show();*/
-    	
-    	
-    	
-    	
     }
     
     
     
     
 }
-   
-    
-	
-  
-
-
-    
- 
-
-
-
-
-
